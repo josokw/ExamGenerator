d83a3f9088a714aa975a276f5b7de8e72fe2dc54
jos <jos.onokiewicz@han.nl>
2018-09-21 17:25:13 +0200

    Added GenComposite

---
+++ examGen/GenComposite.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iterator>
+#include <string>
+
+#include "GenComposite.h"
+#include "Log.h"
+
+GenComposite::GenComposite()
+   : IGenerator()
+   , generators_{}
+{
+   type_ = "GenComposite";
+   LOGD("initialised");
+}
+
+GenComposite::GenComposite(const std::string &id)
+   : IGenerator(id)
+   , generators_{}
+{
+   type_ = "GenComposite";
+   LOGD("initialised");
+}
+
+GenComposite::~GenComposite()
+{
+   // IGenerator::write(clog) << " ##### DTOR COMPOSITE" << endl;
+}
+
+void GenComposite::add(IGenPtr_t pGen)
+{
+   generators_.push_back(pGen);
+}
+
+std::ostream &GenComposite::write(std::ostream &os, int level) const
+{
+   IGenerator::write(os, level);
+   os << "\n";
+   for_each(generators_.begin(), generators_.end(),
+            [&os, &level](IGenPtr_t g) { g->write(os, level + 1); });
+   return os;
+}
+
+void GenComposite::generate(std::ostream &os)
+{
+   for_each(generators_.begin(), generators_.end(), [&os](IGenPtr_t g) {
+      g->prepare();
+      g->generate(os);
+   });
+}
---
+++ examGen/GenComposite.h
@@ -0,0 +1,37 @@
+#ifndef GENCOMPOSITE_H
+#define GENCOMPOSITE_H
+
+#include <iostream>
+#include <vector>
+
+#include "IGenerator.h"
+
+/// Composite generator class (abstract base class),
+/// aggregates pointers to 0..n other generator objects
+/// (instantiations of #IGenerator derived classes).
+/// According to composite design pattern.
+///
+/// Class invariant: #generators_ contains no null pointers.
+class GenComposite : public IGenerator
+{
+public:
+   GenComposite();
+   GenComposite(const std::string &id);
+   virtual ~GenComposite();
+
+   virtual IGenPtr_t copy() const = 0;
+   virtual void add(IGenPtr_t pGen) = 0;
+   virtual void generate(std::ostream &os);
+   virtual std::ostream &write(std::ostream &os, int level = 0) const;
+
+   IGenPtr_t operator[](int index) { return generators_.at(index); }
+   std::vector<IGenPtr_t> &getGenerators() { return generators_; }
+   const std::vector<IGenPtr_t> &getGenerators() const { return generators_; }
+   size_t size() const { return generators_.size(); }
+   void clear() { generators_.clear(); }
+
+protected:
+   std::vector<IGenPtr_t> generators_;
+};
+
+#endif
