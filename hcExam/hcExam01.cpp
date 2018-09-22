a2c236132d698560345f6885047770ef32c322c9
jos <jos.onokiewicz@han.nl>
2018-09-22 11:49:27 +0200

    Deleted

--- hcExam/hcExam01.cpp
+++
@@ -1,8 +0,0 @@
-#include "hcExam01.h"
-#include "Log.h"
-
-void hcExam01(std::ofstream& LaTeXfile)
-{
-    LOGD("Generating LaTeX started");
-
-}
--- hcExam/hcExam01.h
+++
@@ -1,8 +0,0 @@
-#ifndef HCEXAM01_H
-#define HCEXAM01_H
-
-#include <fstream>
-
-void hcExam01(std::ofstream& LaTeXfile);
-
-#endif
