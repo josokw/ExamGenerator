5d2ecfb3a3b2ef4c815a71278f4b755d732077b8
jos <jos.onokiewicz@han.nl>
2018-09-22 11:41:54 +0200

    Added GenSelector

---
+++ examGen/GenSelector.cpp
@@ -0,0 +1,58 @@
+#include "GenSelector.h"
+#include "Log.h"
+
+#include <algorithm>
+#include <iterator>
+#include <string>
+
+GenSelector::GenSelector()
+   : GenComposite()
+{
+   type_ = "GenSelector";
+   LOGD("initialised");
+}
+
+GenSelector::GenSelector(const std::string &id)
+   : GenComposite(id)
+{
+   type_ = "GenSelector";
+   LOGD("initialised");
+}
+
+GenSelector::~GenSelector()
+{
+   // IGenerator::write(clog) << " ##### DTOR" << endl;
+}
+
+IGenPtr_t GenSelector::copy() const
+{
+   std::shared_ptr<GenSelector> p(new GenSelector(*this));
+   for_each(p->generators_.begin(), p->generators_.end(),
+            [](IGenPtr_t &pGen) { pGen = pGen->copy(); });
+   return p;
+}
+
+void GenSelector::add(IGenPtr_t pGen)
+{
+   allGenerators_.push_back(pGen);
+   // for calculation a next permutation
+   std::sort(allGenerators_.begin(), allGenerators_.end());
+}
+
+void GenSelector::selectR(unsigned int n)
+{
+   next_permutation(allGenerators_.begin(), allGenerators_.end());
+   generators_.clear();
+   for (size_t i = 0; i < allGenerators_.size() && i < n; ++i) {
+      generators_.push_back(allGenerators_[i]->copy());
+   }
+}
+
+std::ostream &GenSelector::write(std::ostream &os, int level) const
+{
+   auto size = generators_.size();
+   auto sizeAll = allGenerators_.size();
+   IGenerator::write(os, level);
+   os << ": size ALL = " << sizeAll << " size selected = " << size << "\n";
+   return GenComposite::write(os, level + 1);
+}
---
+++ examGen/GenSelector.h
@@ -0,0 +1,33 @@
+#ifndef GENSELECTOR_H
+#define GENSELECTOR_H
+
+#include "GenComposite.h"
+
+#include <iostream>
+#include <vector>
+
+/// Specialized composite generator class, aggregates 0..n other generator
+/// objects (instantiations of IGenerator derived classes).
+/// Supports random selection of a number of aggregated generator objects.
+///
+/// Class invariant: ??
+class GenSelector : public GenComposite
+{
+public:
+   GenSelector();
+   GenSelector(const std::string &id);
+   virtual ~GenSelector();
+
+   virtual IGenPtr_t copy() const;
+   virtual void add(IGenPtr_t pGen);
+   // virtual void generate(std::ostream& os);
+   virtual std::ostream &write(std::ostream &os, int level = 0) const;
+
+   int sizeAll() const { return allGenerators_.size(); }
+   void selectR(unsigned int n);
+
+protected:
+   std::vector<IGenPtr_t> allGenerators_;
+};
+
+#endif
