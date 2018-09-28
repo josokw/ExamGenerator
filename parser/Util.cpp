278ac559b1bcee82a0a7de1ee607990c5bea7f22
jos <jos.onokiewicz@han.nl>
2018-09-28 07:27:52 +0200

    Scope vars reduced

--- parser/Util.cpp
+++ parser/Util.cpp
@@ -6,7 +6,7 @@
 
 std::string util::Binary(int n, int maxBits)
 {
-   std::string Result;
+   std::string result;
    int nBits = 8 * sizeof(n);
 
    if (nBits > maxBits) {
@@ -15,49 +15,45 @@ std::string util::Binary(int n, int maxBits)
 
    for (int i = nBits - 1; i >= 0; i--) {
       int bit = ((n >> i) & 1);
-      Result += (bit ? "1" : "0");
+      result += (bit ? "1" : "0");
    }
 
-   return Result;
+   return result;
 }
 
 std::vector<std::string> util::toTruthTable(util::bool3Pars_t boolExpr)
 {
-   std::vector<std::string> Result;
-   std::string SubResult;
-   bool b1(false);
-   bool b2(false);
-   bool b3(false);
+   std::vector<std::string> result;
+   std::string subResult;
 
    for (unsigned char i = 0; i < 8; ++i) {
-      b3 = ((i >> 0) & 1) == 1;
-      b2 = ((i >> 1) & 1) == 1;
-      b1 = ((i >> 2) & 1) == 1;
-      SubResult = boost::lexical_cast<std::string>(b1) + " & " +
+      bool b3 = ((i >> 0) & 1) == 1;
+      bool b2 = ((i >> 1) & 1) == 1;
+      bool b1 = ((i >> 2) & 1) == 1;
+
+      subResult = boost::lexical_cast<std::string>(b1) + " & " +
                   boost::lexical_cast<std::string>(b2) + " & " +
                   boost::lexical_cast<std::string>(b3) + " & " +
                   boost::lexical_cast<std::string>(boolExpr(b1, b2, b3)) + " ";
-      Result.push_back(SubResult);
+      result.push_back(subResult);
    }
 
-   return Result;
+   return result;
 }
 
 std::vector<std::string> util::toTruthTable(util::bool2Pars_t boolExpr)
 {
-   std::vector<std::string> Result;
-   std::string SubResult;
-   bool b1(false);
-   bool b2(false);
+   std::vector<std::string> result;
+   std::string subResult;
 
    for (unsigned int i = 0; i < 4; ++i) {
-      b2 = (i >> 1) == 1;
-      b1 = (i >> 2) == 1;
-      SubResult = boost::lexical_cast<std::string>(b1) + " " +
+      bool b2 = (i >> 1) == 1;
+      bool b1 = (i >> 2) == 1;
+      subResult = boost::lexical_cast<std::string>(b1) + " " +
                   boost::lexical_cast<std::string>(b2) + "  " +
                   boost::lexical_cast<std::string>(boolExpr(b1, b2)) + "\n";
-      Result.push_back(SubResult);
+      result.push_back(subResult);
    }
 
-   return Result;
+   return result;
 }
