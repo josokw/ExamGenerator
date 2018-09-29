86bb3aef386a1a2899d367dde3eb2d56c28c24b2
jos <jos.onokiewicz@han.nl>
2018-09-29 17:27:34 +0200

    Removed boost::

--- parser/Util.h
+++ parser/Util.h
@@ -1,7 +1,7 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#include <boost/function.hpp>
+#include <functional>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -10,9 +10,9 @@ namespace util {
 
 std::string Binary(int n, int maxBits);
 
-typedef boost::function<bool(bool, bool, bool)> bool3Pars_t;
-typedef boost::function<bool(bool, bool)> bool2Pars_t;
-typedef boost::function<bool(bool)> bool1Pars_t;
+typedef std::function<bool(bool, bool, bool)> bool3Pars_t;
+typedef std::function<bool(bool, bool)> bool2Pars_t;
+typedef std::function<bool(bool)> bool1Pars_t;
 
 std::vector<std::string> toTruthTable(bool3Pars_t boolExpr);
 std::vector<std::string> toTruthTable(bool2Pars_t boolExpr);
