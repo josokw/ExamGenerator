4fa5d04690e55fd67e9654c54c6bb387a58b3211
jos <jos.onokiewicz@han.nl>
2019-09-19 13:06:33 +0200

    Added rDNA string

--- app/examGen/GenRandom.h
+++ app/examGen/GenRandom.h
@@ -1,9 +1,11 @@
 #ifndef GENRANDOM_H
 #define GENRANDOM_H
 
+#include "AppInfo.h"
 #include "Seed.h"
 
 #include <random>
+#include <string>
 
 /// Responsible for generating pseudo random integer values
 /// in a range [min, max).
@@ -13,9 +15,17 @@ public:
    GenRandom(int incr = 0) { prnEng_.seed(getSeed() + incr); }
    /// Returns min <= random value < max, range [min, max)
    /// \pre max > min
-   int generate(int max, int min = 0) { return prnEng_() % (max - min) + min; }
+   int generate(int max, int min = 0)
+   {
+      auto rnd = prnEng_() % (max - min) + min;
+      rDNA_s += "-" + std::to_string(rnd);
+      return rnd;
+   }
+
+   static const std::string &rDNA() { return rDNA_s; }
 
 private:
+   inline static std::string rDNA_s{"rDNA-" VERSION};
    /// Standard pseudo-random number engine, reproducable, portable
    std::minstd_rand prnEng_;
 };
